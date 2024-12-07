fe1dc79c18c63eacfb93af995f6e60526e8cae7b
Damian Oguche <damianguch@yahoo.com>
2024-12-07 08:28:29 +0100

    Modified the server.js file for production

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
-import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
 
 // Product routes
 app.use('/api/products', productRoutes);
+
+// Compress the frontend in the backend for production
+if (process.env.MODE === 'development') {
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
